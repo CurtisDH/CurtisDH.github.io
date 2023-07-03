97d95ecf62cd2c30bc5a5a8378d2a4b2f9463082
CurtisDH <curtisdh0@gmail.com>
2023-07-03 11:18:22 +1000

    Removed placeholder information

--- Scripts/projectFetcherScript.js
+++ Scripts/projectFetcherScript.js
@@ -1,13 +1,21 @@
-const username = "CurtisDH"; 
+// TODO use this to get all public repos
+// TODO then we need to structure all of our existing repos the same
+// i.e. include an image folder, a README which contains the same structure
+// so we can get the description etc. and include it on the site
+// TODO We also want this only run once every 24 hours or so might need to implement caching or something
+// to avoid spamming the GitHub API when its not required
+// and then update automatically
+// could potentially just setup a different script on a Pi to update the repo
+// TODO project pinning system? as in highlight a specific 4 then have all down on the scroll
+
+const username = "CurtisDH";
 const container = document.getElementById("scrollContainer");
 
 fetch(`https://api.github.com/users/${username}/repos`)
   .then((response) => response.json())
   .then((data) => {
     const repoPromises = data.map((repo) =>
-      fetch(
-        `https://api.github.com/repos/${username}/${repo.name}/README.md`
-      )
+      fetch(`https://api.github.com/repos/${username}/${repo.name}/README.md`)
         .then((response) => {
           if (response.status === 200) {
             return response.json();
