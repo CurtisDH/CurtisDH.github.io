3f6465ce491e3b9ceee07e15004c7ed9481da2b3
CurtisDH <curtisdh0@gmail.com>
2023-07-13 12:16:52 +1000

    Removed scroll elements from exp & edu. Content is now in dot points instead of text block

--- Scripts/Content/education.js
+++ Scripts/Content/education.js
@@ -1,95 +1,131 @@
 class Education {
-  constructor(institution, degree, duration, description, gpa) {
-    this.institution = institution;
-    this.degree = degree;
-    this.duration = duration;
-    this.gpa = gpa;
-    this.description = description;
-  }
-  // TODO add image support
-  async render() {
-    let response = await fetch("../../HTML/Templates/education.html");
-
-    let template = await response.text();
-    let tempDiv = document.createElement("div");
-    tempDiv.innerHTML = template.trim();
-    let element = tempDiv.firstChild;
-
-    // Target and modify the title
-    const institution = element.querySelector(".institution");
-    const degree = element.querySelector(".degree");
-    const duration = element.querySelector(".duration");
-    const gpa = element.querySelector(".gpa");
-    const description = element.querySelector(".desc");
-
-    if (institution) {
-      institution.textContent = this.institution;
+    constructor(institution, degree, duration, descriptions, gpa) {
+        this.institution = institution;
+        this.degree = degree;
+        this.duration = duration;
+        this.descriptions = descriptions;
+        this.gpa = gpa;
     }
 
-    if (degree) {
-      degree.textContent = this.degree;
-    }
+    async render() {
+        let response = await fetch("../../HTML/Templates/education.html");
+        let template = await response.text();
+        let tempDiv = document.createElement("div");
+        tempDiv.innerHTML = template.trim();
+        let element = tempDiv.firstChild;
 
-    if (duration) {
-      duration.textContent = this.duration;
-    }
+        const institution = element.querySelector(".institution");
+        const degree = element.querySelector(".degree");
+        const duration = element.querySelector(".duration");
+        const descriptionContainer = element.querySelector(".desc-container");
+        const gpa = element.querySelector(".gpa");
 
-    if (gpa) {
-      gpa.textContent = this.gpa;
-    }
-    if (description) {
-      description.textContent = this.description;
-    }
+        if (institution) {
+            institution.textContent = this.institution;
+        }
 
-    // Get the scroll-container element
-    const scrollContainer = document.querySelector(".scroll-container");
-    if (scrollContainer) {
-      scrollContainer.append(element);
-    }
+        if (degree) {
+            degree.textContent = this.degree;
+        }
 
-    return element;
-  }
+        if (duration) {
+            duration.textContent = this.duration;
+        }
+
+        if (descriptionContainer && this.descriptions) {
+            this.descriptions.forEach((desc) => {
+                const listItem = document.createElement("li");
+                listItem.textContent = desc;
+                descriptionContainer.appendChild(listItem);
+            });
+        }
+
+        if (gpa) {
+            gpa.textContent = this.gpa;
+        }
+
+        const scrollContainer = document.querySelector(".container-education");
+        if (scrollContainer) {
+            scrollContainer.append(element);
+        }
+
+        return element;
+    }
 }
 
 const education = [
-  new Education(
-    "Queensland University of Technology",
-    "Bachelor of Information Technology - Computer Science",
-    "March 2022 - November 2023",
-    "Majoring in Computer Science, achieved GPA of 7.0 in Discrete Mathematics, Algorithms & Time Complexity, currently undertaking: High Performance Parallel Computing, Cloud Computing (with an AWS focus), and Systems Programming (Focus on low level programming)",
-    "Overall GPA 6.0"
-  ),
-  new Education(
-    "TAFE Queensland",
-    "Diploma of Computer Systems Networking and Telecommunications",
-    "January 2021 - December 2021",
-    "Emphasised a great understand of theoretical and physical computer networking infrastructure. Worked heavily with virtual machines and Linux. Worked heavily with CISCO switches, routers, and wireless access points. Great understanding of network protocols and security implementations",
-    "Diploma Awarded"
-  ),
-  new Education(
-    "TAFE Queensland",
-    "Certificate III in Game and Interactive Media Design",
-    "January 2016 - December 2018",
-    "Completed this Certificate while undertaking High school. The course was focused on design, 3D modelling, media editing (Photoshop, Premier pro, Illustrator etc.) Created a great understanding of how users interact with designed environments & software",
-    "Certificate Awarded"
-  ),
-  new Education(
-    "Cannon Hill Anglican College",
-    "Queensland Certificate of Education & Certificate III in Business",
-    "January 2014 - December 2018",
-    "Grade 12 Highschool education. Also undertook a Certificate III in Business along side a second certificate from TAFE. Queensland Certificate of Education was awarded upon graduation alongside a Certificate III in Business & Certificate III in Game and Interactive Media Design.",
-    "Certificates Awarded"
-  ),
+    new Education(
+        "Queensland University of Technology",
+        "Bachelor of Information Technology - Computer Science",
+        "March 2022 - November 2023",
+        [
+            "Majoring in Computer Science",
+            "Achieved GPA of 7.0 in Discrete Mathematics, Algorithms & Time Complexity",
+            "Currently undertaking: High Performance Parallel Computing, Cloud Computing (with an AWS focus), and Systems Programming (Focus on low level programming)",
+        ],
+        "Overall GPA 6.0"
+    ),
+    new Education(
+        "TAFE Queensland",
+        "Diploma of Computer Systems Networking and Telecommunications",
+        "January 2021 - December 2021",
+        [
+            "Emphasised a great understanding of theoretical and physical computer networking infrastructure",
+            "Worked heavily with virtual machines and Linux",
+            "Worked heavily with CISCO switches, routers, and wireless access points",
+            "Great understanding of network protocols and associated security implementations",
+        ],
+        "Diploma Awarded"
+    ),
+    new Education(
+        "TAFE Queensland",
+        "Certificate III in Game and Interactive Media Design",
+        "January 2016 - December 2018",
+        [
+            "Completed while undertaking high school education",
+            "Covered the following topics:",
+            "Web Design",
+            "3D Modelling",
+            "3D Animation",
+            "Media Editing",
+            "Sound Design",
+            "Photography"
+        ],
+        "Certificate Awarded"
+    ),
+    new Education(
+        "Cannon Hill Anglican College",
+        "Certificate III in Business",
+        "January 2016 - December 2018",
+        [
+            "Completed while undertaking high school education",
+            "Leadership, innovation and creative thinking",
+            "Customer service and teamwork",
+            "Inclusivity and effective communication",
+            "WHS and sustainability",
+            "Business documentation"
+        ],
+        "Certificate Awarded"
+    ),
+    new Education(
+        "Cannon Hill Anglican College",
+        "Queensland Certificate of Education",
+        "January 2014 - December 2018",
+        [
+            "Grade 12 high school education resulting in QCE being awarded",
+            "Undertook Certificate III in Business & Certificate III in Game and Interactive Media Design"],
+        "Certificates Awarded"
+    ),
+
 ];
-Promise.all(education.map((education) => education.render()))
-  .then((renderedEducation) => {
-    const scrollContainer = document.querySelector(
-      ".scroll-container-education"
-    );
-    renderedEducation.forEach((project) => {
-      scrollContainer.append(project);
+
+Promise.all(education.map((edu) => edu.render()))
+    .then((renderedEducation) => {
+        const scrollContainer = document.querySelector(".container-education");
+        renderedEducation.forEach((edu) => {
+            scrollContainer.append(edu);
+        });
+    })
+    .catch((error) => {
+        console.error("Error rendering education:", error);
     });
-  })
-  .catch((error) => {
-    console.error("Error rendering projects:", error);
-  });
--- Scripts/Content/experience.js
+++ Scripts/Content/experience.js
@@ -1,81 +1,94 @@
 class Experience {
-  constructor(company, position, duration, description) {
-    this.company = company;
-    this.position = position;
-    this.duration = duration;
-    this.description = description;
-  }
-  // TODO add image support
-  async render() {
-    let response = await fetch("../../HTML/Templates/experience.html");
-
-    let template = await response.text();
-    let tempDiv = document.createElement("div");
-    tempDiv.innerHTML = template.trim();
-    let element = tempDiv.firstChild;
-
-    // Target and modify the title
-    const institution = element.querySelector(".company");
-    const degree = element.querySelector(".position");
-    const duration = element.querySelector(".duration");
-    const description = element.querySelector(".desc");
-
-    if (institution) {
-      institution.textContent = this.company;
+    constructor(company, position, duration, descriptions) {
+        this.company = company;
+        this.position = position;
+        this.duration = duration;
+        this.descriptions = descriptions;
     }
 
-    if (degree) {
-      degree.textContent = this.position;
-    }
+    async render() {
+        let response = await fetch("../../HTML/Templates/experience.html");
+        let template = await response.text();
+        let tempDiv = document.createElement("div");
+        tempDiv.innerHTML = template.trim();
+        let element = tempDiv.firstChild;
 
-    if (duration) {
-      duration.textContent = this.duration;
-    }
+        const institution = element.querySelector(".company");
+        const degree = element.querySelector(".position");
+        const duration = element.querySelector(".duration");
+        const descriptionContainer = element.querySelector(".desc-container");
 
-    if (description) {
-      description.textContent = this.description;
-    }
+        if (institution) {
+            institution.textContent = this.company;
+        }
 
-    // Get the scroll-container element
-    const scrollContainer = document.querySelector(".scroll-container");
-    if (scrollContainer) {
-      scrollContainer.append(element);
-    }
+        if (degree) {
+            degree.textContent = this.position;
+        }
 
-    return element;
-  }
+        if (duration) {
+            duration.textContent = this.duration;
+        }
+
+        if (descriptionContainer && this.descriptions) {
+            this.descriptions.forEach((desc) => {
+                const listItem = document.createElement("li");
+                listItem.textContent = desc;
+                descriptionContainer.appendChild(listItem);
+            });
+        }
+
+        const scrollContainer = document.querySelector(".container-experience");
+        if (scrollContainer) {
+            scrollContainer.append(element);
+        }
+
+        return element;
+    }
 }
 
 const experience = [
-  new Experience(
-    "Queensland Police - Capstone",
-    "Software Developer",
-    "March 2023 - November 2023",
-    "In conjunction with QUT, the capstone program serves as a multifaceted venture that not only provides valuable work experience but also enables the accumulation of credit points towards my bachelor's degree in computer science. This unique opportunity involves an array of pivotal processes, including but not limited to crafting a comprehensive project report outline encompassing critical aspects such as risk assessment, ethical considerations, and project timelines. Collaboratively liaising with Queensland Police, we have engaged in a meticulous and iterative exchange of ideas, persisting until reaching a mutually agreeable solution. Subsequently, this diligent groundwork has set the stage for the subsequent stages of the development process.",
-    "Overall GPA 6.0"
-  ),
-  new Experience(
-    "YesVR (Internship)",
-    "Software Developer ",
-    "December 2020 - April 2021",
-    "At YesVR, I contributed to the development of immersive VR experiences using Unity and the Unity XR Toolkit. I focused on C# backend development and collaborated with audio designers and web developers. I gained experience with Agile methodologies and worked independently by utilising Asana for project management. Here I also gained significant experience working with a pre-existing code-base, and participating in key client meetings discussing what aspects of the software needed to be adjusted to fit the clients needs"
-  ),
-  new Experience(
-    "GameDevHQ (BootCamp)",
-    "Software Developer",
-    "August 2020 - December 2020",
-    "This BootCamp was focused on Unity & C#, throughout this BootCamp I gained essential skills and knowledge in game development. The boot camp covered program debugging, performance profiling, and optimising software issues. I also learned important programming patterns like Singleton's, Object Pooling, Command, and Chain of Responsibility. These patterns helped me design scalable code structures for robust game systems. During the BootCamp I integrated REST APIs into game environments to interact with external data sources and enhance game mechanics. This experience taught me how to leverage REST APIs for more realistic and functional game experiences. "
-  ),
+    new Experience(
+        "Queensland Police - Capstone",
+        "Software Developer",
+        "March 2023 - November 2023",
+        [
+            "Collaborated with Queensland Police on a comprehensive project for the capstone program",
+            "Heavily involved in risk assessment, ethical considerations, and project timelines",
+            "Contributed to the development process through iterative exchange of ideas until client requirements were satisfied",
+            "Worked extensively with Javascript, HTML5, and CSS3"
+        ]
+    ),
+    new Experience(
+        "YesVR (Internship)",
+        "Software Developer",
+        "December 2020 - April 2021",
+        [
+            "Contributed to the development of immersive VR experiences using Unity and the Unity XR Toolkit",
+            "Focused on C# backend development and collaborated with audio designers and web developers",
+            "Gained experience with Agile methodologies and worked independently utilizing Asana for project management",
+            "Gained experience working with a pre-existing codebase and participating in key client meetings",
+        ]
+    ),
+    new Experience(
+        "GameDevHQ (BootCamp)",
+        "Software Developer",
+        "August 2020 - December 2020",
+        [
+            "Developed essential skills and knowledge in game development",
+            "Learned programming patterns like Singleton's, Object Pooling, Command, and Chain of Responsibility",
+            "Integrated REST APIs into game environments to enhance game mechanics",
+        ]
+    ),
 ];
-Promise.all(experience.map((experience) => experience.render()))
-  .then((renderedExperience) => {
-    const scrollContainer = document.querySelector(
-      ".scroll-container-experience"
-    );
-    renderedExperience.forEach((project) => {
-      scrollContainer.append(project);
+
+Promise.all(experience.map((exp) => exp.render()))
+    .then((renderedExperience) => {
+        const scrollContainer = document.querySelector(".container-experience");
+        renderedExperience.forEach((exp) => {
+            scrollContainer.append(exp);
+        });
+    })
+    .catch((error) => {
+        console.error("Error rendering work experience:", error);
     });
-  })
-  .catch((error) => {
-    console.error("Error rendering projects:", error);
-  });
